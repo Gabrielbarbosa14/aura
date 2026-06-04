e731f8947105a7b0b620bcff46f66957f9cce23a
Gabrielbarbosa14 <terezamariabarbosa635@gmail.com>
2026-06-04 14:40:34 -0300

    Adicionando novas sections

--- script.js
+++ script.js
@@ -45,4 +45,34 @@ function slideAnterior() {
         imagens[contador].style.opacity = "1"
     }, 10)
 }
-console.log(imagens)
+
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsvive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
+});
