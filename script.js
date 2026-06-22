7899822f99c5b554f3ea138c1b66659603e8baec
Gabrielbarbosa14 <terezamariabarbosa635@gmail.com>
2026-06-22 11:30:32 -0300

    Aumentando o espaçamento entre as sections, melhorando a section de FAQ e adicionando a lógica dela no JS

--- script.js
+++ script.js
@@ -1,78 +1,95 @@
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 0) {
-        nav.classList.add('sticky-nav');
-    } else {
-        nav.classList.remove('sticky-nav');
-    }
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    nav.classList.add("sticky-nav");
+  } else {
+    nav.classList.remove("sticky-nav");
+  }
 });
 
-let proximo = document.querySelector("#proximo")
-let anterior = document.querySelector("#anterior")
-let imagens = document.querySelectorAll(".axd-img")
+let proximo = document.querySelector("#proximo");
+let anterior = document.querySelector("#anterior");
+let imagens = document.querySelectorAll(".axd-img");
 
-let contador = 0
+let contador = 0;
 
 function proximoSlide() {
-    document.querySelector(".img0").classList.remove("img0")
+  document.querySelector(".img0").classList.remove("img0");
 
-    if(contador < 5){
-        ++contador
-    } else{
-        contador = 0
-    }
+  if (contador < 5) {
+    ++contador;
+  } else {
+    contador = 0;
+  }
 
-    imagens[contador].classList.add("img0")
-    imagens[contador].style.opacity = "0"
-    setTimeout(() => {
-        imagens[contador].style.opacity = "1"
-    }, 10)
+  imagens[contador].classList.add("img0");
+  imagens[contador].style.opacity = "0";
+  setTimeout(() => {
+    imagens[contador].style.opacity = "1";
+  }, 10);
 }
 
 function slideAnterior() {
-    document.querySelector(".img0").classList.remove("img0")
+  document.querySelector(".img0").classList.remove("img0");
 
-    if(contador == 0){
-        contador = 5
-    } else{
-        --contador
-    }
+  if (contador == 0) {
+    contador = 5;
+  } else {
+    --contador;
+  }
 
-    imagens[contador].classList.add("img0")
-    imagens[contador].style.opacity = "0"
-    setTimeout(() => {
-        imagens[contador].style.opacity = "1"
-    }, 10)
+  imagens[contador].classList.add("img0");
+  imagens[contador].style.opacity = "0";
+  setTimeout(() => {
+    imagens[contador].style.opacity = "1";
+  }, 10);
 }
 
-new Swiper('.card-wrapper', {
+new Swiper(".card-wrapper", {
   loop: true,
   spaceBetween: 30,
 
   // pagination bullets
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // Responsvive breakpoints
   breakpoints: {
     0: {
-        slidesPerView: 1
+      slidesPerView: 1,
     },
     768: {
-        slidesPerView: 2
+      slidesPerView: 2,
     },
     1024: {
-        slidesPerView: 3
+      slidesPerView: 3,
     },
-  }
+  },
+});
+
+// Lógica do FAQ
+let perguntas = document.querySelectorAll(".perguntas");
+
+perguntas.forEach((itens) => {
+  itens.addEventListener("click", () => {
+    let taAtiva = itens.classList.contains("ativa");
+
+    perguntas.forEach((outrasPerguntas) => {
+      outrasPerguntas.classList.remove("ativa");
+    });
+
+    if (!taAtiva) {
+      itens.classList.add("ativa");
+    }
+  });
 });
